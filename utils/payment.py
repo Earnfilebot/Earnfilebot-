c0b0fb3a94c79566ea78cbb06a0a486596dbdeaa
Earnfilebot <tzyfile22@gmail.com>
2026-06-14 19:06:51 +0700

    Create payment.py

---
+++ utils/payment.py
@@ -0,0 +1,25 @@
+import httpx
+from config import BAYARGG_API_KEY, BAYARGG_BASE_URL
+
+
+async def create_bayargg_invoice(amount: int, code: str, user_id: int):
+
+    payload = {
+        "amount": amount,
+        "description": f"Purchase file {code}",
+        "callback_url": "earnfilebot.railway.internal.com/webhook/bayargg",
+        "external_id": f"{user_id}_{code}"
+    }
+
+    headers = {
+        "Authorization": f"Bearer {BAYARGG_API_KEY}"
+    }
+
+    async with httpx.AsyncClient() as client:
+        r = await client.post(
+            f"{BAYARGG_BASE_URL}/transaction/create",
+            json=payload,
+            headers=headers
+        )
+
+    return r.json()
