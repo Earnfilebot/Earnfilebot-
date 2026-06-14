55ea439f43e07897f05343ebf951c89cb1fdf823
Earnfilebot <tzyfile22@gmail.com>
2026-06-14 14:55:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,13 +7,15 @@ from database import connect_db
 
 from handlers.check_sub import router as check_sub_router
 from handlers.start import router as start_router
+from handlers import start, check_sub, upfile
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(start_router)
 dp.include_router(check_sub_router)
-
+dp.include_router(upfile.router
+                  
 async def main():
     await connect_db()
     await dp.start_polling(bot)
