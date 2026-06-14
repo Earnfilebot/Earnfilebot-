8f7097e390ea0bca3b932084e24cda4a5bb063c7
Earnfilebot <tzyfile22@gmail.com>
2026-06-14 16:37:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from database import connect_db
 from handlers.check_sub import router as check_sub_router
 from handlers.start import router as start_router
 from handlers.upfile import router as upfile_router
+from handlers.getfile import router as getfile_router
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
@@ -15,6 +16,7 @@ dp = Dispatcher()
 dp.include_router(start_router)
 dp.include_router(check_sub_router)
 dp.include_router(upfile_router)
+dp.include_router(getfile_router)
                   
 async def main():
     await connect_db()
