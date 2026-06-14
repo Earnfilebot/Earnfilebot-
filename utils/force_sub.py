5fb23e32fc0099b5dd674036ab7ee1b54b926a98
Earnfilebot <tzyfile22@gmail.com>
2026-06-14 12:06:53 +0700

    Create force_sub.py

---
+++ utils/force_sub.py
@@ -0,0 +1,29 @@
+from aiogram import Bot
+
+CHANNELS = [
+    -1003712587847,
+    -1003721009353
+]
+
+async def check_force_sub(
+    bot: Bot,
+    user_id: int
+):
+    for channel in CHANNELS:
+        try:
+            member = await bot.get_chat_member(
+                channel,
+                user_id
+            )
+
+            if member.status not in (
+                "member",
+                "administrator",
+                "creator"
+            ):
+                return False
+
+        except:
+            return False
+
+    return True
