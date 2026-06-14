2c4aff5c9ace2591361ce96adfc0e80b582ac14c
Earnfilebot <tzyfile22@gmail.com>
2026-06-14 23:19:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import connect_db
 
+from utils.payment import router as payment_router
 from handlers.check_sub import router as check_sub_router
 from handlers.start import router as start_router
 from handlers.upfile import router as upfile_router
@@ -17,6 +18,7 @@ dp.include_router(start_router)
 dp.include_router(check_sub_router)
 dp.include_router(upfile_router)
 dp.include_router(getfile_router)
+dp.include_router(payment_router)
                   
 async def main():
     await connect_db()
