b7db1081a279e22fc91ba9f9be03610fb3ee5c7c
Earnfilebot <tzyfile22@gmail.com>
2026-06-14 14:58:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,20 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 🔥 VALIDASI WAJIB (biar error ketahuan dari awal)
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN belum di-set di .env")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL belum di-set di .env")
+
+
+CHANNEL_ID = -1003721009353  # group post
+
+
+# =========================
+# PRICE NORMALIZER
+# =========================
+def normalize_price(text: str):
+    return int("".join(filter(str.isdigit, text)))
