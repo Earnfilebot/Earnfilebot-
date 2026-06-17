78ec639b00363f0b43727d82221ff26422e865c9
Earnfilebot <tzyfile22@gmail.com>
2026-06-17 07:22:53 +0700

    Update database.py

--- database.py
+++ database.py
@@ -8,11 +8,11 @@ async def connect_db():
 
     global pool
 
-    if pool:
+    if pool is not None:
         return pool
 
     pool = await asyncpg.create_pool(
-        DATABASE_URL,
+        dsn=DATABASE_URL,
         min_size=1,
         max_size=20,
         command_timeout=60
@@ -29,3 +29,13 @@ async def get_pool():
         await connect_db()
 
     return pool
+
+
+async def close_db():
+
+    global pool
+
+    if pool is not None:
+
+        await pool.close()
+        pool = None
