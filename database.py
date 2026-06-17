00b6524be7ab1992628575f6623e9c4127672bfb
Earnfilebot <tzyfile22@gmail.com>
2026-06-17 07:20:06 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,9 +3,29 @@ from config import DATABASE_URL
 
 pool = None
 
+
 async def connect_db():
+
     global pool
-    pool = await asyncpg.create_pool(DATABASE_URL)
+
+    if pool:
+        return pool
+
+    pool = await asyncpg.create_pool(
+        DATABASE_URL,
+        min_size=1,
+        max_size=20,
+        command_timeout=60
+    )
+
+    return pool
+
 
 async def get_pool():
+
+    global pool
+
+    if pool is None:
+        await connect_db()
+
     return pool
