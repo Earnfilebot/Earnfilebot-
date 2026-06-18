cecce4183b3ee1edfb0c5ef85260ffd3283102f9
Earnfilebot <tzyfile22@gmail.com>
2026-06-18 12:03:33 +0700

    Update qr.py

--- utils/qr.py
+++ utils/qr.py
@@ -2,11 +2,20 @@ import qrcode
 from io import BytesIO
 from aiogram.types import BufferedInputFile
 
+
 def generate_qr_image(qris_string: str):
-    qr = qrcode.make(qris_string)
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=2
+    )
+    qr.add_data(qris_string)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
 
     buffer = BytesIO()
-    qr.save(buffer, format="PNG")
+    img.save(buffer, format="PNG")
     buffer.seek(0)
 
     return BufferedInputFile(buffer.read(), filename="qris.png")
