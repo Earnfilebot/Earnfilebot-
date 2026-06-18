f31cf654820ae5289beae5ff73aa45aabb11678f
Earnfilebot <tzyfile22@gmail.com>
2026-06-18 13:08:07 +0700

    Update qr.py

--- utils/qr.py
+++ utils/qr.py
@@ -1,21 +1,27 @@
 import qrcode
 from io import BytesIO
-from aiogram.types import BufferedInputFile
 
 
-def generate_qr_image(qris_string: str):
-    qr = qrcode.QRCode(
-        version=1,
-        box_size=10,
-        border=2
-    )
-    qr.add_data(qris_string)
-    qr.make(fit=True)
+def generate_qr_image(data: str):
+    try:
+        qr = qrcode.QRCode(
+            version=1,
+            box_size=10,
+            border=2
+        )
 
-    img = qr.make_image(fill_color="black", back_color="white")
+        qr.add_data(data)
+        qr.make(fit=True)
 
-    buffer = BytesIO()
-    img.save(buffer, format="PNG")
-    buffer.seek(0)
+        img = qr.make_image(fill_color="black", back_color="white")
 
-    return BufferedInputFile(buffer.read(), filename="qris.png")
+        bio = BytesIO()
+        bio.name = "qris.png"
+        img.save(bio, "PNG")
+        bio.seek(0)
+
+        return bio
+
+    except Exception as e:
+        print("QR GENERATE ERROR:", e)
+        return None
