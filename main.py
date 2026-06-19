352828b3217195183986313bc5fe485920d3267a
Earnfilebot <tzyfile22@gmail.com>
2026-06-19 15:26:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,38 @@
 from fastapi import FastAPI
-import logging
 from contextlib import asynccontextmanager
+import logging
 
 from bot import bot, dp
 from webhook.bayargg import router as bayargg_router
 from database import get_pool, close_db
 
 app = FastAPI()
-
-# =========================
-# ROUTES
-# =========================
 app.include_router(bayargg_router)
 
+logging.basicConfig(level=logging.INFO)
 
-# =========================
-# LIFESPAN
-# =========================
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
-    logging.info("🚀 STARTING APP...")
+    logging.info("🚀 STARTING APP")
 
     await get_pool()
 
     app.state.bot = bot
     app.state.dp = dp
 
-    logging.info("✅ DATABASE CONNECTED")
-    logging.info("🤖 BOT READY (WEBHOOK MODE)")
+    logging.info("✅ BOT READY")
 
     yield
 
     await close_db()
     await bot.session.close()
 
-    logging.info("🛑 BOT STOPPED")
-
+    logging.info("🛑 STOPPED")
 
 app.router.lifespan_context = lifespan
 
 
-# =========================
-# HEALTH CHECK
-# =========================
 @app.get("/")
 async def root():
-    return {"status": "ok", "bot": "running"}
+    return {"status": "ok"}
