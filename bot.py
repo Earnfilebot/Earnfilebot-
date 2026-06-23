414467ed1614bb37d2af7327ad38cbe4c8266eba
Earnfilebot <tzyfile22@gmail.com>
2026-06-23 12:54:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,6 @@ from handlers.start import router as start_router
 from handlers.check_sub import router as check_sub_router
 from handlers.upfile import router as upfile_router
 from handlers.getfile import router as getfile_router
-from handlers.buy import router as buy_router
 from handlers.page import router as page_router
 from handlers.account import router as account_router
 from handlers.help import router as help_router
@@ -28,7 +27,6 @@ dp.include_router(start_router)
 dp.include_router(check_sub_router)
 dp.include_router(upfile_router)
 dp.include_router(getfile_router)
-dp.include_router(buy_router)
 dp.include_router(page_router)
 dp.include_router(account_router)
 dp.include_router(help_router)
