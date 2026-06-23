3b273f50d3978516c1f7d8d2014c812907391d0a
Earnfilebot <tzyfile22@gmail.com>
2026-06-23 12:49:02 +0700

    Delete utils directory

--- utils/guard.py
+++
@@ -1,4 +0,0 @@
-from utils.force_sub import check_force_sub
-
-async def force_guard(bot, user_id):
-    return await check_force_sub(bot, user_id)
--- utils/helpers.py
+++
@@ -1,6 +0,0 @@
-def parse_reference(ref: str):
-    try:
-        user_id, code = ref.split("_", 1)
-        return int(user_id), code
-    except:
-        return None, None
