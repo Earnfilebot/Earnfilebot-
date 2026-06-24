bac928d0c187cc1c83f3fd800efbe63611ff5f10
Earnfilebot <tzyfile22@gmail.com>
2026-06-24 13:16:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ from aiogram.client.default import DefaultBotProperties
 
 from config import BOT_TOKEN
 
+# =========================
+# BOT INIT
+# =========================
 bot = Bot(
     BOT_TOKEN,
     default=DefaultBotProperties(parse_mode="HTML")
@@ -11,7 +14,7 @@ bot = Bot(
 dp = Dispatcher()
 
 # =========================
-# ROUTERS
+# ROUTERS IMPORT
 # =========================
 from handlers.start import router as start_router
 from handlers.check_sub import router as check_sub_router
@@ -19,16 +22,23 @@ from handlers.upfile import router as upfile_router
 from handlers.getfile import router as getfile_router
 from handlers.page import router as page_router
 from handlers.account import router as account_router
+from handlers.code import router as code_router
+from handlers.vvip import router as vvip_router
 from handlers.help import router as help_router
 from handlers.about import router as about_router
 from handlers.admin import router as admin_router
 
+# =========================
+# REGISTER ROUTERS
+# =========================
 dp.include_router(start_router)
 dp.include_router(check_sub_router)
 dp.include_router(upfile_router)
 dp.include_router(getfile_router)
 dp.include_router(page_router)
 dp.include_router(account_router)
+dp.include_router(code_router)
+dp.include_router(vvip_router)
 dp.include_router(help_router)
 dp.include_router(about_router)
 dp.include_router(admin_router)
