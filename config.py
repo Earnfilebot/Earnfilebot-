d4919ce3cbafbafae54df2433a68d4663346cced
Earnfilebot <tzyfile22@gmail.com>
2026-06-29 05:52:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,45 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# =========================
+# GENERAL
+# =========================
 TIMEZONE = "Asia/Jakarta"
+
 # =========================
 # BOT
 # =========================
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_USERNAME = "DecoderFileBot"
+
 # =========================
 # DATABASE
 # =========================
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # =========================
+# PAYMENT
+# =========================
+BAYARGG_API_KEY = os.getenv("BAYARGG_API_KEY")
+BAYARGG_MERCHANT = os.getenv("BAYARGG_MERCHANT")
+
+# =========================
 # CHANNEL / GROUP
 # =========================
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1004395938795"))
 GROUP_ID = int(os.getenv("GROUP_ID", str(CHANNEL_ID)))
-ADMIN_IDS = list(
-    map(int, os.getenv("ADMIN_IDS", "").split(","))
-)
+
+# =========================
+# ADMIN
+# =========================
+ADMIN_IDS = [
+    int(x)
+    for x in os.getenv("ADMIN_IDS", "").split(",")
+    if x.strip()
+]
+
 # =========================
 # VALIDATION
 # =========================
