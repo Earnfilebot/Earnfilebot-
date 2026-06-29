78e9efa0ddcaf626398ca7a480ee8b615452bd5e
Earnfilebot <tzyfile22@gmail.com>
2026-06-29 12:29:43 +0700

    Update config_vip.py

--- config_vip.py
+++ config_vip.py
@@ -1,47 +1,56 @@
 # =========================
-# VIP PACKAGE CONFIG
+# VIP PACKAGES
 # =========================
 
 VIP_PACKAGES = {
+
     "vip1": {
+        "id": "vip1",
+        "name": "💎 VIP 1 Hari",
         "days": 1,
         "price": 15000,
-        "name": "VIP 1 Hari"
     },
 
     "vip3": {
+        "id": "vip3",
+        "name": "💎 VIP 3 Hari",
         "days": 3,
         "price": 25000,
-        "name": "VIP 3 Hari"
     },
 
     "vip5": {
+        "id": "vip5",
+        "name": "💎 VIP 5 Hari",
         "days": 5,
         "price": 35000,
-        "name": "VIP 5 Hari"
     },
 
     "vip7": {
+        "id": "vip7",
+        "name": "💎 VIP 7 Hari",
         "days": 7,
         "price": 45000,
-        "name": "VIP 7 Hari"
     },
 
     "vip10": {
+        "id": "vip10",
+        "name": "💎 VIP 10 Hari",
         "days": 10,
         "price": 50000,
-        "name": "VIP 10 Hari"
     },
 
     "vip20": {
+        "id": "vip20",
+        "name": "💎 VIP 20 Hari",
         "days": 20,
         "price": 70000,
-        "name": "VIP 20 Hari"
     },
 
     "vip30": {
+        "id": "vip30",
+        "name": "💎 VIP 30 Hari",
         "days": 30,
         "price": 100000,
-        "name": "VIP 30 Hari"
     },
+
 }
