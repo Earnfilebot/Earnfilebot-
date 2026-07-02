80030df6f9e45b9a65fa346016fbacb66818a171
Earnfilebot <tzyfile22@gmail.com>
2026-07-03 00:16:52 +0700

    Update check_sub.py

--- handlers/check_sub.py
+++ handlers/check_sub.py
@@ -6,6 +6,7 @@ from aiogram.types import CallbackQuery
 from utils.force_sub import check_force_sub
 from keyboards.join import join_kb
 from handlers.start import render_home_fast
+from database import get_pool
 
 router = Router()
 
@@ -33,9 +34,26 @@ async def check_sub_callback(call: CallbackQuery):
             )
             return
 
+        pool = await get_pool()
+
+        user = await pool.fetchrow(
+            """
+            SELECT username, balance
+            FROM users
+            WHERE telegram_id=$1
+            """,
+            user_id
+        )
+
         await call.answer("✅ Verifikasi berhasil")
 
-        await render_home_fast(call.bot, call.message, user_id)
+        await render_home_fast(
+            call.bot,
+            call.message,
+            user_id,
+            user["username"] or "unknown",
+            user["balance"] or 0
+        )
 
     except Exception as e:
         logging.exception(f"CHECK SUB ERROR: {e}")
