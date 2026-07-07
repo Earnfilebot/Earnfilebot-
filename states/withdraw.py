ddd5fba09399e146d5582543f55276426fa7acf9
Earnfilebot <tzyfile22@gmail.com>
2026-07-07 20:38:20 +0700

    Update withdraw.py

--- states/withdraw.py
+++ states/withdraw.py
@@ -6,26 +6,32 @@ class WithdrawState(StatesGroup):
     # =========================
     # SET REKENING / E-WALLET
     # =========================
+
     select_method = State()
     input_account_number = State()
     input_account_name = State()
 
+
     # =========================
     # WITHDRAW REGULER
     # =========================
+
     select_account = State()
     input_amount = State()
     confirm = State()
 
+
     # =========================
     # WITHDRAW INSTANT
     # =========================
+
     instant_select_account = State()
     instant_confirm = State()
-
-    input_account_number = State()
-    input_account_name = State()
-
-    input_withdraw_amount = State()
     input_instant_amount = State()
+
+
+    # =========================
+    # EDIT REKENING
+    # =========================
+
     edit_account_name = State()
