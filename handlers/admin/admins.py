0d545d0158a2eb77b1731ce1648d4873ea4c134b
Earnfilebot <tzyfile22@gmail.com>
2026-07-08 05:23:50 +0700

    Update admins.py

--- handlers/admin/admins.py
+++ handlers/admin/admins.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from config import OWNER_ID, ADMINS
+from config import ADMIN_IDS
 
 router = Router()
 
@@ -8,25 +8,4 @@ router = Router()
 # CHECK ADMIN
 # =========================
 def is_admin(user_id: int) -> bool:
-    """
-    Cek apakah user adalah owner/admin
-    """
-
-    admin_list = set()
-
-    # OWNER
-    if OWNER_ID:
-        try:
-            admin_list.add(int(OWNER_ID))
-        except:
-            pass
-
-    # ADMINS dari env
-    if ADMINS:
-        for x in ADMINS.split(","):
-            x = x.strip()
-
-            if x.isdigit():
-                admin_list.add(int(x))
-
-    return user_id in admin_list
+    return user_id in ADMIN_IDS
